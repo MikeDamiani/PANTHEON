509cf99840beb21d92741cdee3c4d17779084aea
MikeDamiani <michaeldamianibusiness@gmail.com>
2023-09-14 04:09:29 +0200

    Events fixed

--- PANTHEON.sol
+++ PANTHEON.sol
@@ -23,10 +23,10 @@ contract PANTHEON is ERC20Burnable, Ownable2Step, ReentrancyGuard {
 
     uint128 public constant ETHinWEI = 1 * 10 ** 18;
 
-    event Price(uint256 time, uint256 recieved, uint256 sent);
-    event MaxUpdated(uint256 max);
-    event RedeemFeeUpdated(uint256 redeemFee);
-    event MintFeeUpdated(uint256 mintFee);
+    event PriceAfterMint(uint256 time, uint256 recieved, uint256 sent);
+    event PriceAfterRedeem(uint256 time, uint256 recieved, uint256 sent);
+    event FeeAddressUpdated(address newFeeAddress);
+    event totalEthFixed(uint256 totalEthEmergencyFixed);
 
     constructor(address _feeAddress) payable ERC20("Pantheon", "PANTHEON") {
         if(_feeAddress == address(0)) revert ZeroAddressNotAllowed();
@@ -42,6 +42,8 @@ contract PANTHEON is ERC20Burnable, Ownable2Step, ReentrancyGuard {
     function setFeeAddress(address _address) external onlyOwner {
         require(_address != address(0x0));
         FEE_ADDRESS = payable(_address);
+
+        emit FeeAddressUpdated(_address);
     }
 
     function redeem(uint256 pantheon) external nonReentrant {
@@ -63,7 +65,7 @@ contract PANTHEON is ERC20Burnable, Ownable2Step, ReentrancyGuard {
 
         totalEth -= (ethToSender + ethToFeeAddress);
 
-        emit Price(block.timestamp, pantheon, eth);
+        emit PriceAfterRedeem(block.timestamp, pantheon, eth);
     }
     
     function mint(address reciever) external payable nonReentrant {
@@ -79,7 +81,7 @@ contract PANTHEON is ERC20Burnable, Ownable2Step, ReentrancyGuard {
 
         totalEth += (msg.value - ethToFeeAddress);
 
-        emit Price(block.timestamp, pantheon, msg.value);
+        emit PriceAfterMint(block.timestamp, pantheon, msg.value);
     }
 
     function PANTHEONtoETH(uint256 value) public view returns (uint256) {
@@ -115,6 +117,8 @@ contract PANTHEON is ERC20Burnable, Ownable2Step, ReentrancyGuard {
 
     function emergencyFixTotalEth() external onlyOwner nonReentrant {
         totalEth = address(this).balance;
+
+        emit totalEthFixed(address(this).balance);
     }
 
     receive() external payable {
